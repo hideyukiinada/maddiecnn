604a4cdcf8db219197fc06122263db2f524c9e6a
Hide Inada <hideyuki@gmail.com>
2018-11-09 18:08:12 -0800

    Documented config.py

--- project/config.py
+++ project/config.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
-'''Identify my 4 dogs using CNN based on Keras.
+'''Load config in JSON
 
-
+__author__ = "Hide Inada"
+__copyright__ = "Copyright 2018, Hide Inada"
+__license__ = "The MIT license"
+__email__ = "hideyuki@gmail.com"
 '''
 
 import simplejson as json
 
 
-
 def load_config():
+    """Load config in JSON.
+
+    Returns
+    -------
+    config: dictionary
+        Configuration with key value pairs
+
+    Note
+    ----
+    This function removes comments in JSON data for the caller to be able to access the config value without specifying
+    the "value" key.
+    """
+
     with open("config.json") as f:
         config_raw = json.load(f)
 
